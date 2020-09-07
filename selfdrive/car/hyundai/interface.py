73422e70e14a7c0d88feb801175fa8a8277f5742
Chanho Jung <jungch@naver.com>
2020-09-07 16:07:29 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -64,9 +64,6 @@ class CarInterface(CarInterfaceBase):
     ret.steerRateCost = 0.5
     ret.steerLimitTimer = 0.4
 
-
-
-
     if candidate == CAR.GRANDEUR_H_19:
       ret.lateralTuning.pid.kf = 0.000005      
       ret.mass = 1675. + STD_CARGO_KG
@@ -74,8 +71,36 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 12.5  #12.5
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+    if candidate == CAR.GRANDEUR_H_20:
+      ret.steerActuatorDelay = 0.08  
+      ret.steerLimitTimer = 0.8 # stock is 0.01 but 0.04 seems to work well
+      tire_stiffness_factor = 1.0
+      ret.steerRateCost = 0.5
+      ret.mass = 1675. + STD_CARGO_KG
+      ret.wheelbase = 2.885
+      ret.steerRatio = 13.0  #12.5
+      
+      #ret.lateralTuning.pid.kf = 0.000005      
+      #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
+      #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.20], [0.05]]
+      #lqr
+      ret.lateralTuning.init('lqr')
+      ret.lateralTuning.lqr.scale = 2500.0
+      ret.lateralTuning.lqr.ki = 0.05
+      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+      ret.lateralTuning.lqr.c = [1., 0.]
+      ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+      ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
+      ret.lateralTuning.lqr.dcGain = 0.002237852961363602
+      # indi
+      #ret.lateralTuning.init('indi')
+      #ret.lateralTuning.indi.innerLoopGain = 3.0
+      #ret.lateralTuning.indi.outerLoopGain = 2.0
+      #ret.lateralTuning.indi.timeConstant = 1.0
+      #ret.lateralTuning.indi.actuatorEffectiveness = 1.5
 
-    elif candidate == CAR.SANTA_FE:
+      elif candidate == CAR.SANTA_FE:
       ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 3982. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.766
